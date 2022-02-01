10605ab6ded385820284dca55d45ba8bf4d50f39
yashadesara <96791091+yashadesara@users.noreply.github.com>
2022-02-01 10:44:35 +0530

    Update 11.C

--- C/exe2/11.C
+++ C/exe2/11.C
@@ -19,3 +19,24 @@ void main()
 	s1[j] = '\0';
 	printf("%s",s1);
 }
+
+
+// #include<stdio.h>
+// #include<string.h>
+
+// int main()
+// {
+// 	int i,j,n=0;
+// 	char str[10],str2[10];
+	
+// 	printf("Enter s string: ");
+// 	gets(str);
+		
+// 	for(i=0; str[i]!='\0'; i++)
+// 		n++;
+		
+// 	for(j=n-1; j>=0; j--)
+// 	{
+// 		printf("%c",str[j]);	
+// 	}
+// }
