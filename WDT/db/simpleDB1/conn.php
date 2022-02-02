5523cba0a966022df91f65c7a39592ebdf2ade83
yashadesara <96791091+yashadesara@users.noreply.github.com>
2022-02-02 14:59:44 +0530

    Delete WDT/db directory

--- WDT/db/simpleDB1/conn.php
+++
@@ -1,8 +0,0 @@
-<?php
-$mysqli = mysqli_connect("localhost", "root", "", "jvims");
-
-if (mysqli_connect_errno()) {
-	printf("Connect failed: %s\n", mysqli_connect_error());
-	exit();
-} 
-?>
--- WDT/db/simpleDB1/index.php
+++
@@ -1,6 +0,0 @@
-<form action="insert.php" method="POST">
-<br/>First Name : <input type="text" name="fname" id="fname">
-<br/>Middle Name : <input type="text" name="mname" id="mname">
-<br/>Last Name : <input type="text" name="lname" id="lname">
-<br/><input type="submit" value="Insert">
-</form>
